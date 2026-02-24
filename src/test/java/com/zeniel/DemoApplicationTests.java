45549354e07447c7e105b1a38d3c5b000cbf1a12
Galmaeki <kjh09800@gmail.com>
2026-02-24 13:32:14 +0900

    Init Commit

---
+++ src/test/java/com/zeniel/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeniel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
