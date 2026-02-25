a94f396fa06784228b5a3a7adf1bf26c7b79e613
YONGMINJO1 <iy0594@naver.com>
2026-02-25 11:48:38 +0900

    feat: ClientRepository 이름+주민번호 내담자 조회 메서드 추가

--- src/main/java/com/zeniel/repository/ClientRepository.java
+++ src/main/java/com/zeniel/repository/ClientRepository.java
@@ -1,7 +1,10 @@
 package com.zeniel.repository;
 
 import com.zeniel.entity.client.Clients;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ClientRepository extends JpaRepository<Clients, Long> {
+
+    Optional<Clients> findByNameAndResidentId(String name, String residentId);
 }
