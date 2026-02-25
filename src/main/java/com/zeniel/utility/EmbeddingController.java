772765227bc2632534fada0107a752d2b52fd848
LeeHyeongju <a01028245126@gmail.com>
2026-02-25 15:18:07 +0900

    chore: clients 테이블에 벡터 컬럼 추가

---
+++ src/main/java/com/zeniel/utility/EmbeddingController.java
@@ -0,0 +1,29 @@
+package com.zeniel.utility;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class EmbeddingController {
+    @Autowired
+    private EmbeddingService embeddingService;
+
+    @GetMapping("/")
+    public String embedding(@RequestParam("clientId") Long clientId) {
+        embeddingService.saveConsultation(clientId);
+        return "콘솔 확인";
+    }
+
+    @PutMapping("/embedding")
+    public String embedding() {
+        for (Long i = 1L; i <= 150L; i++) {
+            embeddingService.saveConsultation(i);
+        }
+        return "콘솔 확인";
+    }
+    
+}
